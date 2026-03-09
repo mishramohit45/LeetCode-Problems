476ed6acdee055a0089e1401a8d2602e9555f6d2
Mohit Kumar Mishra <149972919+mohitmishra45@users.noreply.github.com>
2026-03-09 18:00:00 +0530

    Time: 0 ms (100%), Space: 21.7 MB (92.57%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n=nums.size();
-        int nSum=n*(n+1)/2;
-        int sum=0;
-        for(int i=0;i<n;i++)
-        {
-            sum+=nums[i];
-        }
-        return nSum-sum;   
+     int n=nums.size();
+     int xor1=0;
+     int xor2=0;
+
+     //Step1:Find Xor of all no. in expected Range
+     for(int i=0;i<=n;i++)
+     {
+        xor1^=i;
+     }   
+
+     //Step2:Find Xor of all the elements of array
+     for(int i=0;i<n;i++)
+     {
+        xor2^=nums[i];
+     }
+
+     return xor1^xor2;
     }
 };
