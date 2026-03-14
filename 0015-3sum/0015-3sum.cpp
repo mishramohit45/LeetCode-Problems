4faef804f52b475d89236aa2f00e7755b4dce36a
Mohit Kumar Mishra <149972919+mohitmishra45@users.noreply.github.com>
2026-03-14 14:52:23 +0530

    Time: 76 ms (15.13%), Space: 115.2 MB (8.67%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>>result;
+    void twoSum(vector<int>nums,int target,int i,int j)
+    {
+        while(i<j)
+        {
+            int sum=nums[i]+nums[j];
+            if(sum>target) j--;
+            else if(sum<target) i++;
+            else
+            {
+                while(i<j && nums[i]==nums[i+1]) i++;
+                while(i<j && nums[j]==nums[j-1]) j--;
+                result.push_back({-target,nums[i],nums[j]});
+                i++;
+                j--;
+            }
+        }
+    }
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        if(nums.size()<3) return {};
+        //Sorting
+        sort(nums.begin(),nums.end());
+        result.clear();
+        //Fixing One Value
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i>0 && nums[i]==nums[i-1]) continue;
+
+        int n1=nums[i];
+        int target=-n1;
+        twoSum(nums,target,i+1,nums.size()-1);
+        }
+        return result;
+    }
+};
