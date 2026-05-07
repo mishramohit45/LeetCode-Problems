713878d4e8b8c58df6f29c3ded35a415bfdcc7fd
Mohit Kumar Mishra <mohitmishra9707@gmail.com>
2026-05-07 22:08:10 +0530

    Time: 1 ms (72.79%), Space: 10 MB (44.2%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+        int k=s1.size();
         vector<int>sfreq(26,0);
         vector<int>pfreq(26,0);
-        for(int i=0;i<s1.size();i++)
+        for(int i=0;i<k;i++)
         {
             pfreq[s1[i]-'a']++;
         }
@@ -12,7 +13,7 @@ public:
             sfreq[s2[i]-'a']++;
             if(i>=s1.size())
             {
-                sfreq[s2[i-s1.size()]-'a']--;
+                sfreq[s2[i-k]-'a']--;
             }
             if(sfreq==pfreq) return true;
         }
