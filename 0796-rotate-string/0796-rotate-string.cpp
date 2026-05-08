f0717e861c8cbb44ea3262c5fcd2d81373fbb590
Mohit Kumar Mishra <mohitmishra9707@gmail.com>
2026-05-09 03:05:32 +0530

    Time: 0 ms (100%), Space: 8 MB (82.87%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
+        //1)first check both size if unequal then return false
+        //2)second adding the same string to it self bc all roatation exist if we add like s=abcdeabcde goal=cdeab
+        //3)last chek in the string that goal is exist or not
         if(s.size()!=goal.size()) return false;
         string temp=s+s;
         if(temp.find(goal)!=-1) return true;
