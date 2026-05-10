46da055f907a0d392b96d69f0a02b04c15273c15
Mohit Kumar Mishra <mohitmishra9707@gmail.com>
2026-05-11 02:27:52 +0530

    Time: 0 ms (100%), Space: 8.6 MB (19.51%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long rev=0;
+        while(x!=0)
+        {
+            int ld=x%10;
+            rev=rev*10+ld;
+            x=x/10;
+        }
+        if(rev<INT_MIN || rev>INT_MAX)
+        {
+            return 0;
+        }
+        else
+        {
+        return rev;
+        }
+    }
+};
