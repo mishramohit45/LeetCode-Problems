a20b4e01561c6f7a5280355c3d1d505e2daf34a1
Mohit Kumar Mishra <mohitmishra9707@gmail.com>
2026-05-18 03:21:00 +0530

    Time: 3 ms (14.43%), Space: 9 MB (74.84%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
+    bool isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+               ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+    }
+
     bool halvesAreAlike(string s) {
-        string f1="";
-        string f2="";
 
-        int n=s.size();
-        int i=0;
-        int j=n/2;
+        int count1 = 0;
+        int count2 = 0;
 
-        while(i<n/2)
-        {
-            f1+=s[i];
-            i++;
-        }
-        while(j<n)
-        {
-            f2+=s[j];
-            j++;
-        }
-        int count1=0;
-        int count2=0;
+        int n = s.size();
 
-        for(int i=0;i<f1.size();i++)
-        {
-            if(f1[i]=='a' || f1[i]=='e' || f1[i]=='i' || f1[i]=='o' || f1[i]=='u' || f1[i]=='A' || f1[i]=='E' || f1[i]=='I' || f1[i]=='O' || f1[i]=='U')
-            {
+        for (int i = 0; i < n / 2; i++) {
+            if (isVowel(s[i])) {
                 count1++;
             }
-        }
-    
-    for(int i=0;i<f1.size();i++)
-        {
-            if(f2[i]=='a' || f2[i]=='e' || f2[i]=='i' || f2[i]=='o' || f2[i]=='u'
-            || f2[i]=='A' || f2[i]=='E' || f2[i]=='I' || f2[i]=='O' || f2[i]=='U')
-            {
+
+            if (isVowel(s[i + n / 2])) {
                 count2++;
             }
         }
-    return count1==count2;
+
+        return count1 == count2;
     }
 };
