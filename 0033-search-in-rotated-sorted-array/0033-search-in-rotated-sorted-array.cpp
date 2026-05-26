b043158fd75daf828ba60f31c1f80997290e2aa8
Mohit Kumar Mishra <mohitmishra9707@gmail.com>
2026-05-27 01:32:56 +0530

    Time: 0 ms (100%), Space: 15.2 MB (33.68%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,25 +1,60 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] == target)
-                return mid;
-
-            if (nums[l] <= nums[mid]) {
-                if (target >= nums[l] && target < nums[mid])
-                    r = mid - 1;
-                else
-                    l = mid + 1;
+    int bs(vector<int>&nums,int target,int start,int end)
+    {
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target)
+            {
+                end=mid-1;
             }
-            else {
-                if (target > nums[mid] && target <= nums[r])
-                    l = mid + 1;
-                else
-                    r = mid - 1;
+            else if(nums[mid]<target)
+            {
+                start=mid+1;
             }
         }
         return -1;
+    }
+    int minIndex(vector<int>&nums)
+    {
+        int n=nums.size();
+        int start=0;
+        int end=n-1;
+        while(start<=end)
+        {
+            if(nums[start]<=nums[end])
+            {
+                return start;
+            }
+            int mid=start+(end-start)/2;
+            int next=(mid+1)%n;
+            int prev=(mid+n-1)%n;
+            if(nums[mid]<nums[prev] && nums[mid]<nums[next])
+            {
+                return mid;
+            }
+            else if(nums[start]<=nums[mid])
+            {
+                start=mid+1;
+            }
+            else if(nums[mid]<=nums[end])
+            {
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int idx=minIndex(nums);
+        int a1=bs(nums,target,0,idx-1);
+        int a2=bs(nums,target,idx,n-1);
+         if(a1 != -1)
+            return a1;
+
+        return a2;
     }
 };
