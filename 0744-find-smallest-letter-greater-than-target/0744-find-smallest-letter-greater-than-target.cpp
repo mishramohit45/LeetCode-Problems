f790f0a7dd3b55b918dcd98fb13559163406a233
Mohit Kumar Mishra <mohitmishra9707@gmail.com>
2026-05-27 02:57:43 +0530

    Time: 0 ms (100%), Space: 19.7 MB (61.15%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -3,21 +3,22 @@ public:
     char nextGreatestLetter(vector<char>& letters, char target) {
         int low=0;
         int high=letters.size()-1;
-
+        char res=letters[0];
         while(low<=high)
         {
             int mid=low+(high-low)/2;
-            if(letters[mid]<=target)
+            if(letters[mid]>target)
             {
-                low=mid+1;
+                res=letters[mid];
+                high=mid-1;
             }
             else
             {
-                high=mid-1;
+                low=mid+1;
             }
             
         }
-        return letters[low % letters.size()];
+        return res;
     }
 
 };
